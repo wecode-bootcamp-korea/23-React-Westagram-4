081a304c8433a5e47aa1e1db3649b3fd1e9bb323
xia4 <77831217+xia4@users.noreply.github.com>
2021-07-21 16:12:27 +0900

    Add: first commit. 초기 세팅 완료.

---
+++ src/Routes.js
@@ -0,0 +1,37 @@
+import LoginSoo from './pages/jinsooso/Login/Login';
+import LoginHyun from './pages/hyunjunlee/Login/Login';
+import LoginNa from './pages/nahyunlee/Login/Login';
+import LoginSu from './pages/sujeonglee/Login/Login';
+
+import MainSoo from './pages/jinsooso/Main/Main';
+import MainNa from './pages/nahyunlee/Main/Main';
+import MainSu from './pages/sujeonglee/Main/Main';
+import MainHyun from './pages/hyunjunlee/Main/Main'
+import React from 'react';
+import { Router, Switch } from 'react-router';
+
+
+
+class Routes extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                <Route exact path='/login-jinsoo' component={LoginSoo} />
+                <Route exact path='/main-jinsoo' component={MainSoo} />
+
+                <Route exact path='/login-nahyun' component={LoginNa}/>
+                <Route exact path='/main-nahyun' component={MainNa}/>
+
+                <Route exact path='/login-sujeong' component={LoginSu} />
+                <Route exact path='/main-sujeong' component={MainSu} /> 
+
+                <Route exact path="/login-hyunjun" component={LoginHyun} />
+                <Route exact path="/Main-hyunjun" component={MainHyun} />
+                </Switch>
+            </Router>
+        )
+    }
+}
+    
+export default Routes;
